c08c3c1d11bee3a1794791bd5a4b5799626ecbb3
Anmol Parande <parandea1.7@gmail.com>
2019-01-13 12:54:55 -0600

    Weak classifiers are now perceptrons Updated Documentation

--- viola_jones.py
+++ viola_jones.py
@@ -4,22 +4,17 @@ Viola, Paul, and Michael Jones. "Rapid object detection using a boosted cascade 
 Works in both Python2 and Python3
 """
 import numpy as np
-from neuralnetwork import NeuralNetwork as NN
 import math
 from mnist import load_data
 import pickle
 
 class ViolaJones:
-    def __init__(self, feature_num = 10, learning_rate=0.01, regularizer = 0):
+    def __init__(self, feature_num = 10):
         """
           Args:
             feature_num: The number of weak classifiers which should be used
-            learning_rate: How fast the weak classifiers will learn
-            regularizer: whether or not the weak classifiers will be regularized
         """
         self.feature_num = feature_num
-        self.learning_rate = learning_rate
-        self.regularizer = regularizer
         self.alphas = []
         self.clfs = []
 
@@ -41,11 +36,12 @@ class ViolaJones:
                 weights[0][x] = 1.0 / (2 * neg_num)
 
         print("Starting training")
-        weak_classifiers = self.train_weak(training_data)
+        features = self.build_features(training_data[0][0].shape)
 
         for t in range(self.feature_num):
             print("Choosing classifier #%d" % (t+1))
             weights[t] = weights[t] / np.linalg.norm(weights[t])
+            weak_classifiers = self.train_weak(features, weights[t], training_data)
             clf, error, accuracy = self.select_best(weak_classifiers, weights[t], training_data)
             beta = error / (1.0 - error)
             new_weights = np.zeros(weights[t].shape)
@@ -55,65 +51,96 @@ class ViolaJones:
             self.alphas.append(math.log(1.0/beta))
             self.clfs.append(clf)
 
-    def train_weak(self, training_data):
+    def train_weak(self, features, weights, training_data):
         """
-        Trains a weak classifier for each possible feature
+        Finds the optimal thresholds for each weak classifier given the current weights
           Args:
+            features: an array of tuples. Each tuple's first element is an array of the rectangle regions which positively contribute to the feature. The second element is an array of rectangle regions negatively contributing to the feature
+            weights: an array of weights for each training example
             training_data: An array of tuples. The first element is the numpy array of shape (m, n) representing the integral image. The second element is its classification (1 or 0)
           Returns:
             An array of weak classifiers
         """
-        def create_weak(positive_regions, negative_regions):
-            """
-            Trains a weak classifier on the feature
-              Args:
-                positive_regions: Rectangle regions which contribute positively to the feature
-                negative_regions: Rectangle regions which contribute negatively to the feature
-              Returns:
-                The weak classifier
-            """
-            clf = WeakClassifier([1, 2], positive_regions, negative_regions, regularizer = self.regularizer)
-            clf.train(training, training_rate=self.learning_rate)
-            return clf
+        total_pos, total_neg = 0, 0
+        for w, ex in zip(weights, training_data):
+            if ex[1] == 1:
+                total_pos += w
+            else:
+                total_neg += w
 
-        height, width = training_data[0][0].shape
         classifiers = []
+        for positive_regions, negative_regions in features:
+            if len(classifiers) % 1000 == 0:
+                print("Trained %d classifiers" % len(classifiers))
+            feature = lambda ii: sum([pos.compute_feature(ii) for pos in positive_regions]) - sum([neg.compute_feature(ii) for neg in negative_regions])
+            training = map(lambda data: (feature(data[0]), data[1]), training_data)
+            training = sorted(zip(weights, training), key=lambda ex: ex[1][0])
+
+            pos_seen, neg_seen = 0, 0
+            pos_weights, neg_weights = 0, 0
+            min_error, best_feature, best_threshold, best_polarity = float('inf'), None, None, None
+            for w, ex in training:
+                if ex[1] == 1:
+                    pos_seen += 1
+                    pos_weights += w
+                else:
+                    neg_seen += 1
+                    neg_weights += w
+                
+                error = min(neg_weights+total_pos-pos_weights, pos_weights+total_neg-neg_weights)
+                if error < min_error:
+                    min_error = error
+                    best_feature = (positive_regions, negative_regions)
+                    best_threshold = ex[0]
+                    best_polarity = 1 if pos_seen < neg_seen else -1
+            
+            clf = WeakClassifier(best_feature[0], best_feature[1], best_threshold, best_polarity)
+            classifiers.append(clf)
+        return classifiers
+                
+    def build_features(self, image_shape):
+        """
+        Builds the possible features given an image shape
+          Args:
+            image_shape: a tuple of form (height, width)
+          Returns:
+            an array of tuples. Each tuple's first element is an array of the rectangle regions which positively contribute to the feature. The second element is an array of rectangle regions negatively contributing to the feature
+        """
+        height, width = image_shape
+        features = []
         for w in range(1, width+1):
             for h in range(1, height+1):
                 i = 0
                 while i + w < width:
                     j = 0
                     while j + h < height:
-                        if len(classifiers) % 100 == 0 and len(classifiers) != 0:
-                            #print("%d classifiers trained" % len(classifiers))
-                            return classifiers
                         #2 rectangle features
                         immediate = RectangleRegion(i, j, w, h)
                         right = RectangleRegion(i+w, j, w, h)
                         if i + 2 * w < width: #Horizontally Adjacent
-                            classifiers.append(create_weak([right], [immediate]))
+                            features.append(([right], [immediate]))
 
                         bottom = RectangleRegion(i, j+h, w, h)
                         if j + 2 * h < height: #Vertically Adjacent
-                            classifiers.append(create_weak([immediate], [bottom]))
+                            features.append(([immediate], [bottom]))
                         
                         right_2 = RectangleRegion(i+2*w, j, w, h)
                         #3 rectangle features
                         if i + 3 * w < width: #Horizontally Adjacent
-                            classifiers.append(create_weak([right], [right_2, immediate]))
+                            features.append(([right], [right_2, immediate]))
 
                         bottom_2 = RectangleRegion(i, j+2*h, w, h)
                         if j + 3 * h < height: #Vertically Adjacent
-                            classifiers.append(create_weak([bottom], [bottom_2, immediate]))
+                            features.append(([bottom], [bottom_2, immediate]))
 
                         #4 rectangle features
                         bottom_right = RectangleRegion(i+w, j+h, w, h)
                         if i + 2 * w < width and j + 2 * h < height:
-                            classifiers.append(create_weak([right, bottom], [immediate, bottom_right]))
+                            features.append(([right, bottom], [immediate, bottom_right]))
 
                         j += 1
                     i += 1
-        return classifiers
+        return features
 
     def select_best(self, classifiers, weights, training_data):
         """
@@ -121,6 +148,7 @@ class ViolaJones:
           Args:
             classifiers: An array of weak classifiers
             weights: An array of weights corresponding to each training example
+            training_data: An array of tuples. The first element is the numpy array of shape (m, n) representing the integral image. The second element is its classification (1 or 0)
           Returns:
             A tuple containing the best classifier, its error, and an array of its accuracy
         """
@@ -128,7 +156,7 @@ class ViolaJones:
         for clf in classifiers:
             error, accuracy = 0, []
             for data, w in zip(training_data, weights):
-                correctness = abs(np.argmax(clf.classify(data[0])) - np.argmax(data[1]))
+                correctness = abs(clf.classify(data[0]) - data[1])
                 accuracy.append(correctness)
                 error += w * correctness
             if error < best_error:
@@ -138,11 +166,15 @@ class ViolaJones:
     def classify(self, image):
         """
         Classifies an image
+          Args:
+            image: A numpy 2D array of shape (m, n) representing the image
+          Returns:
+            1 if the image is positively classified and 0 otherwise
         """
         total = 0
         ii = integral_image(image)
         for alpha, clf in zip(self.alphas, self.clfs):
-            total += alpha * np.argmax(clf.classify(ii))
+            total += alpha * clf.classify(ii)
         return 1 if total >= 0.5 * sum(self.alphas) else 0
 
     def save(self, filename):
@@ -164,21 +196,31 @@ class ViolaJones:
         with open(filename+".pkl", 'r') as f:
             return pickle.load(f)
 
-class WeakClassifier(NN):
-    def __init__(self, layer_dims, positive_regions, negative_regions, regularizer = 0):
-        NN.__init__(self, layer_dims, regularizer=regularizer)
+class WeakClassifier:
+    def __init__(self, positive_regions, negative_regions, threshold, polarity):
+        """
+          Args:
+            positive_regions: An array of RectangleRegions which positively contribute to a feature
+            negative_regions: An array of RectangleRegions which negatively contribute to a feature
+            threshold: The threshold for the weak classifier
+            polarity: The polarity of the weak classifier
+        """
         self.positive_regions = positive_regions
         self.negative_regions = negative_regions
+        self.threshold = threshold
+        self.polarity = polarity
     
-    def train(self, training_data, batch_size=50, epochs = 5, training_rate = 0.01):
-        feature = lambda ii: sum([pos.compute_feature(ii) for pos in self.positive_regions]) - sum([neg.compute_feature(ii) for neg in self.negative_regions])
-        training = map(lambda data: (feature(data[0]), one_hot(2, data[1])), training_data)
-        return NN.train(self, training, batch_size=batch_size, epochs=epochs, training_rate=training_rate, test_data=None, verbose=False)
-
     def classify(self, x):
-        feature = lambda ii: sum([pos.compute_feature(ii) for pos in self.positive_regions]) - sum([neg.compute_feature(ii) for neg in self.negative_regions])
-        x = feature(x)
-        return NN.classify(self, x) 
+        """
+        Classifies an integral image based on a feature f and the classifiers threshold and polarity
+          Args:
+            x: A 2D numpy array of shape (m, n) representing the integral image
+          Returns:
+            1 if polarity * feature(x) < polarity * threshold
+            0 otherwise
+        """
+        feature = feature = lambda ii: sum([pos.compute_feature(ii) for pos in self.positive_regions]) - sum([neg.compute_feature(ii) for neg in self.negative_regions])
+        return 1 if self.polarity * feature(x) < self.polarity * self.threshold else 0
 
 class RectangleRegion:
     def __init__(self, x, y, width, height):
@@ -213,22 +255,9 @@ def integral_image(image):
     s = np.zeros(image.shape)
     for y in range(len(image)):
         for x in range(len(image[y])):
-            s[y][x] = s[y-1][x] + ii[y][x] if y-1 >= 0 else ii[y][x]
+            s[y][x] = s[y-1][x] + image[y][x] if y-1 >= 0 else image[y][x]
             ii[y][x] = ii[y][x-1]+s[y][x] if x-1 >= 0 else s[y][x]
     return ii
-
-def one_hot(length, label):
-    """
-    Transforms a numeric label into a one-hot vector
-      Args:
-        length: number of classes
-        label: the classification
-      Returns:
-        One-hot vector representation of the label
-    """
-    e = np.zeros((length, 1))
-    e[label] = 1.0
-    return e
 
 if __name__ == "__main__":
     training, validation, test = load_data()
@@ -246,10 +275,10 @@ if __name__ == "__main__":
                 pos_num += 1
             else:
                 neg_num += 1
-        clf = ViolaJones()
+        clf = ViolaJones(feature_num=2)
         clf.train(training, pos_num, neg_num)
 
-        clf.save("viola_jones")
+        #clf.save("viola_jones")
 
     correct = 0
     for x, y in training:
