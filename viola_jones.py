8bd44fff05d272ea9dab55de1ae09b7d7d783b12
Anmol Parande <parandea1.7@gmail.com>
2019-01-16 16:20:58 -0600

    bug fixes and updated documentation to ViolaJones

--- viola_jones.py
+++ viola_jones.py
@@ -10,12 +10,12 @@ import pickle
 from sklearn.feature_selection import SelectPercentile, f_classif
 
 class ViolaJones:
-    def __init__(self, feature_num = 10):
+    def __init__(self, T = 10):
         """
           Args:
             feature_num: The number of weak classifiers which should be used
         """
-        self.feature_num = feature_num
+        self.T = T
         self.alphas = []
         self.clfs = []
 
@@ -27,14 +27,14 @@ class ViolaJones:
             pos_num: the number of positive samples
             neg_num: the number of negative samples
         """
-        weights = np.zeros((1, len(training_data)))
+        weights = np.zeros(len(training_data))
         print("Computing integral images")
         for x in range(len(training_data)):
             training_data[x] = (integral_image(training_data[x][0]), training_data[x][1])
             if training_data[x][1] == 1:
-                weights[0][x] = 1.0 / (2 * pos_num)
+                weights[x] = 1.0 / (2 * pos_num)
             else:
-                weights[0][x] = 1.0 / (2 * neg_num)
+                weights[x] = 1.0 / (2 * neg_num)
 
         print("Building features")
         features = self.build_features(training_data[0][0].shape)
@@ -46,31 +46,26 @@ class ViolaJones:
         features = features[indices]
         print("Selected %d potential features" % len(X))
 
-        for t in range(self.feature_num):
-            print("Choosing classifier #%d" % (t+1))
-            weights[t] = weights[t] / np.linalg.norm(weights[t])
-            weak_classifiers = self.train_weak(X, y, features, weights[t])
-            clf, error, accuracy = self.select_best(weak_classifiers, weights[t], training_data)
+        for t in range(self.T):
+            weights = weights / np.linalg.norm(weights)
+            weak_classifiers = self.train_weak(X, y, features, weights)
+            clf, error, accuracy = self.select_best(weak_classifiers, weights, training_data)
             beta = error / (1.0 - error)
-            new_weights = np.zeros(weights[t].shape)
             for i in range(len(accuracy)):
-                new_weights[i] = weights[t][i] * (beta ** (1 - accuracy[i]))
-            weights = np.vstack((weights, new_weights))
+                weights[i] = weights[i] * (beta ** (1 - accuracy[i]))
             alpha = math.log(1.0/beta)
             self.alphas.append(alpha)
             self.clfs.append(clf)
-            print("Chose classifier: %s with accuracy: %f and alpha: %d" % (str(clf), len(accuracy) - sum(accuracy), alpha))
-
-        print("Alphas are: ", self.alphas)
-        print("Weights: ", weights)
+            print("Chose classifier: %s with accuracy: %f and alpha: %f" % (str(clf), len(accuracy) - sum(accuracy), alpha))
 
     def train_weak(self, X, y, features, weights):
         """
         Finds the optimal thresholds for each weak classifier given the current weights
           Args:
+            X: A numpy array of shape (len(features), len(training_data)). Each row represents the value of a single feature for each training example
+            y: A numpy array of shape len(training_data). The ith element is the classification of the ith training example
             features: an array of tuples. Each tuple's first element is an array of the rectangle regions which positively contribute to the feature. The second element is an array of rectangle regions negatively contributing to the feature
-            weights: an array of weights for each training example
-            training_data: An array of tuples. The first element is the numpy array of shape (m, n) representing the integral image. The second element is its classification (1 or 0)
+            weights: A numpy array of shape len(training_data). The ith element is the weight assigned to the ith training example
           Returns:
             An array of weak classifiers
         """
@@ -93,19 +88,19 @@ class ViolaJones:
             pos_weights, neg_weights = 0, 0
             min_error, best_feature, best_threshold, best_polarity = float('inf'), None, None, None
             for w, f, label in applied_feature:
+                error = min(neg_weights + total_pos - pos_weights, pos_weights + total_neg - neg_weights)
+                if error < min_error:
+                    min_error = error
+                    best_feature = features[index]
+                    best_threshold = f
+                    best_polarity = 1 if pos_seen > neg_seen else -1
+
                 if label == 1:
                     pos_seen += 1
                     pos_weights += w
                 else:
                     neg_seen += 1
                     neg_weights += w
-                
-                error = min(neg_weights + total_pos - pos_weights, pos_weights + total_neg - neg_weights)
-                if error < min_error:
-                    min_error = error
-                    best_feature = features[index]
-                    best_threshold = f
-                    best_polarity = 1 if pos_seen < neg_seen else -1
             
             clf = WeakClassifier(best_feature[0], best_feature[1], best_threshold, best_polarity)
             classifiers.append(clf)
@@ -165,19 +160,28 @@ class ViolaJones:
           Returns:
             A tuple containing the best classifier, its error, and an array of its accuracy
         """
-        best_clf, best_error, best_accuracy = classifiers[0], float('inf'), []
+        best_clf, best_error, best_accuracy = None, float('inf'), None
         for clf in classifiers:
             error, accuracy = 0, []
             for data, w in zip(training_data, weights):
                 correctness = abs(clf.classify(data[0]) - data[1])
                 accuracy.append(correctness)
                 error += w * correctness
-            error = error / len(training_data)
+            #error = error / len(training_data)
             if error < best_error:
                 best_clf, best_error, best_accuracy = clf, error, accuracy
         return best_clf, best_error, best_accuracy
     
     def apply_features(self, features, training_data):
+        """
+        Maps features onto the training dataset
+          Args:
+            features: an array of tuples. Each tuple's first element is an array of the rectangle regions which positively contribute to the feature. The second element is an array of rectangle regions negatively contributing to the feature
+            training_data: An array of tuples. The first element is the numpy array of shape (m, n) representing the integral image. The second element is its classification (1 or 0)
+          Returns:
+            X: A numpy array of shape (len(features), len(training_data)). Each row represents the value of a single feature for each training example
+            y: A numpy array of shape len(training_data). The ith element is the classification of the ith training example
+        """
         X = np.zeros((len(features), len(training_data)))
         y = np.array(map(lambda data: data[1], training_data))
         i = 0
@@ -243,7 +247,7 @@ class WeakClassifier:
             1 if polarity * feature(x) < polarity * threshold
             0 otherwise
         """
-        feature = feature = lambda ii: sum([pos.compute_feature(ii) for pos in self.positive_regions]) - sum([neg.compute_feature(ii) for neg in self.negative_regions])
+        feature = lambda ii: sum([pos.compute_feature(ii) for pos in self.positive_regions]) - sum([neg.compute_feature(ii) for neg in self.negative_regions])
         return 1 if self.polarity * feature(x) < self.polarity * self.threshold else 0
     
     def __str__(self):
@@ -270,6 +274,8 @@ class RectangleRegion:
 
     def __str__(self):
         return "(x= %d, y= %d, width= %d, height= %d)" % (self.x, self.y, self.width, self.height)
+    def __repr__(self):
+        return "RectangleRegion(%d, %d, %d, %d)" % (self.x, self.y, self.width, self.height)
         
 def integral_image(image):
     """
@@ -288,31 +294,3 @@ def integral_image(image):
             s[y][x] = s[y-1][x] + image[y][x] if y-1 >= 0 else image[y][x]
             ii[y][x] = ii[y][x-1]+s[y][x] if x-1 >= 0 else s[y][x]
     return ii
-
-if __name__ == "__main__":
-    training, validation, test = load_data()
-    training = zip(training[0], training[1])
-    training = [(np.reshape(image, (28, 28)), label) for image, label in training if label == 1 or label == 0]
-    training = training[0:1000]
-        
-    try:
-        clf = ViolaJones.load("viola_jones")
-        print("Loaded classifier from file")
-    except Exception as e:
-        print("Failed to load classifier from file: %s" % str(e))
-        pos_num, neg_num = 0, 0
-        for x, y in training:
-            if y == 1:
-                pos_num += 1
-            else:
-                neg_num += 1
-        clf = ViolaJones(feature_num=2)
-        clf.train(training, pos_num, neg_num)
-
-        #clf.save("viola_jones")
-
-    correct = 0
-    for x, y in training:
-        prediction = clf.classify(x)
-        correct += prediction == y
-    print("Correctly labeled %d out of %d training examples" % (correct, len(training)))
